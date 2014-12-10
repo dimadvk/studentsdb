d694c582d4fc2b8259764d4661b895574f3a55f1
manonduty <ren-kpi@i.ua>
2014-12-10 14:30:35 +0200

    add students app to our project

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'studentsdb.views.home', name='home'),
+    # Students urls
+    url(r'^$', 'students.test.students_list', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
 )
