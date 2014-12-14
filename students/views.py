12f05fcbdd9a9ac074157e4b6cd9039b8b67233e
manonduty <ren-kpi@i.ua>
2014-12-14 16:56:59 +0200

    reconstruct the students_list template, make it dynamic

--- students/views.py
+++ students/views.py
@@ -1,10 +1,29 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for Students
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+	{'id': 1,
+	 'first_name': u'Віталій',
+	 'last_name': u'Подоба',
+	 'ticket': 235,
+	 'image': 'img/1.jpg'},
+	{'id': 2,
+	 'first_name': u'Андрій',
+	 'last_name': u'Корост',
+	 'ticket': 2123,
+	 'image': 'img/2.jpg'},
+	{'id': 3,
+	 'first_name': u'Антон',
+	 'last_name': u'Разгільдяй',
+	 'ticket': 512,
+	 'image': 'img/3.gpj'}
+    )
+    return render(request, 'students/students_list.html', {'students':students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
