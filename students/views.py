37d0141a6fc4b3db88a88d8250a5246084c943aa
manonduty <ren-kpi@i.ua>
2014-12-16 17:38:58 +0200

    add base.html, groups_list.html

--- students/views.py
+++ students/views.py
@@ -37,7 +37,19 @@ def students_delete(request, sid):
 # Views for Groups
 
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    groups = (
+        {'id':1,
+	 'name': u'МтМ-21',
+         'headman': u'Віталій Подоба'},
+        {'id': 2,
+	 'name': u'МтМ-22',
+         'headman': u'Андрій Корост'},
+        {'id': 3,
+	 'name': u'МтМ-23',
+         'headman': u'Антон Разгільдяй'}
+    )
+    return render(request, 'students/groups_list.html', {'groups':groups})
+
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
