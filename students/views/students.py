cda78fd06609a49f30f8df387dacb423390953f1
manonduty <ren-kpi@i.ua>
2015-06-18 18:24:32 +0300

    динамізувано посторінкову навігацію для списку судентів

--- students/views/students.py
+++ students/views/students.py
@@ -2,17 +2,39 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from ..models import Student
 
 # Views for Students
 
 def students_list(request):
     students = Student.objects.all()
+
+    # try to order students_list
     order_by = request.GET.get('order_by', '')
     if order_by in ('last_name', 'first_name', 'ticket'):
 	students = students.order_by(order_by)
 	if request.GET.get('reverse', '') == '1':
 	    students = students.reverse()
+    else:
+	order_by = 'last_name'
+	students = students.order_by(order_by)
+
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+	students = paginator.page(page)
+    except PageNotAnInteger:
+	# if page is not an integer, deliver first page.
+	students = paginator.page(1)
+    except EmptyPage:
+	# if page is out of range (e.g. 9999), deliver
+	# last page of results.
+	students = paginator.page(paginator.num_pages)
+
+
     return render(request, 'students/students_list.html', {'students':students})
 
 def students_add(request):
