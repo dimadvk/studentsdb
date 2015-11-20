5a66aff4d2a8fce584005236431fea7a467a7c82
Dimitriy Kutnyakov <ren-kpi@i.ua>
2015-11-20 16:00:54 +0200

    custom pagination

--- students/views/students.py
+++ students/views/students.py
@@ -21,19 +21,47 @@ def students_list(request):
        order_by = 'last_name'
        students = students.order_by(order_by)
 
-    # paginate students
-    paginator = Paginator(students, 3)
+    # # paginate students
+    # paginator = Paginator(students, 3)
+    # page = request.GET.get('page')
+    # try:
+    #     students = paginator.page(page)
+    # except PageNotAnInteger:
+    #     # if page is not an integer, deliver first page.
+    #     students = paginator.page(1)
+    # except EmptyPage:
+    #     # if page is out of range (e.g. 9999), deliver
+    #     # last page of results.
+    #     students = paginator.page(paginator.num_pages)
+    # return render(request, 'students/students_list.html', {'students':students})
+
+    # remake pagination without 'paginator'
+    # count of pages:
+    num_rows_per_page = 3
+    num_pages = len(students) // num_rows_per_page
+    # if len(students) % num_rows_per_page > 0
+    if len(students) % num_rows_per_page:
+        num_pages += 1
     page = request.GET.get('page')
+    # ceck if page is integer
     try:
-        students = paginator.page(page)
-    except PageNotAnInteger:
-        # if page is not an integer, deliver first page.
-        students = paginator.page(1)
-    except EmptyPage:
-        # if page is out of range (e.g. 9999), deliver
-        # last page of results.
-        students = paginator.page(paginator.num_pages)
-    return render(request, 'students/students_list.html', {'students':students})
+        page = int(page)
+    except:
+        page = 1
+    # if page is out of range return last page
+    if page > num_pages:
+        page = num_pages
+    elif page < 1:
+        page = 1
+    students = students[ (page-1)*num_rows_per_page : page*num_rows_per_page ]
+    page_list = [p+1 for p in range(num_pages)]
+    return render(request, 'students/students_list.html',
+            {'students':students,
+            'page':page,
+            'num_pages':num_pages,
+            'num_rows_per_page':num_rows_per_page,
+            'page_list':page_list}
+            )
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
@@ -43,4 +71,3 @@ def students_edit(request, sid):
 
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s' % sid)
-
