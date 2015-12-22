e76c302cec26f99ecd24310b89714af2f9eb0c4e
Dimitriy Kutnyakov <ren-kpi@i.ua>
2015-12-22 18:59:01 +0200

    add validation leader<>group

--- students/admin.py
+++ students/admin.py
@@ -1,8 +1,25 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.forms import ModelForm, ValidationError
+
 from .models.student import Student
 from .models.group import Group
 
-from django.core.urlresolvers import reverse
+
+class StudentFormAdmin(ModelForm):
+
+    def clean_student_group(self):
+        """ Check if student is leader in any group
+        If yes, then ensure it's the same as selected group."""
+        # get group where current student is a leader
+        groups = Group.objects.filter(leader=self.instance)
+        if len(groups) > 0 and self.cleaned_data['student_group'] != groups[0]:
+            raise ValidationError(u'Студент є старостою іншої групи',
+                code='invalid')
+
+        return self.cleaned_data['student_group']
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['last_name', 'first_name', 'ticket', 'student_group']
@@ -12,6 +29,7 @@ class StudentAdmin(admin.ModelAdmin):
     list_filter = ['student_group']
     list_per_page = 10
     search_fields = ['last_name', 'first_name', 'middle_name', 'ticket', 'notes']
+    form  = StudentFormAdmin
 
     def view_on_site(slef, obj):
         return reverse('students_edit', kwargs={'pk':obj.id})
