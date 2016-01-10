67abc7d6670daa7ac5e92174e76a6c524ae9638a
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-01-10 19:13:15 +0200

    comment import Student

--- students/models/group.py
+++ students/models/group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from student import Student
+#from student import Student
 
 # Create your models here
 
@@ -9,23 +9,23 @@ class Group(models.Model):
     '''Group Model'''
 
     class Meta(object):
-        verbose_name = u"Група"
-        verbose_name_plural = u"Групи"
+        verbose_name = u"група"
+        verbose_name_plural = u"групи"
 
     title = models.CharField(
         max_length=256,
         blank=False,
-        verbose_name=u"Назва")
+        verbose_name=u"назва")
 
     leader = models.OneToOneField('Student',
-        verbose_name=u"Староста",
+        verbose_name=u"староста",
         null = True,
         blank = True,
         on_delete = models.SET_NULL)
     
     notes = models.TextField(
         blank=True,
-        verbose_name=u"Додаткові нотатки")
+        verbose_name=u"додаткові нотатки")
 
     def __unicode__(self):
         if self.leader:
