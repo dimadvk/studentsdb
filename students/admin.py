3d7119e54b4e4ee180bf2d86a374d18d981eb962
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-01-28 13:40:46 +0200

    customized admin-page for groups

--- students/admin.py
+++ students/admin.py
@@ -35,7 +35,29 @@ class StudentAdmin(admin.ModelAdmin):
         return reverse('students_edit', kwargs={'pk':obj.id})
 
 
+class GroupFormAdmin(ModelForm):
+
+    def clean_leader(self):
+        """ Check if leader is in the same group """
+        queryset = Student.objects.filter(student_group=self.instance)
+        if self.cleaned_data['leader'] not in queryset:
+            raise ValidationError(u"Студент не входить до даної групи!")
+        return self.cleaned_data['leader']
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader']
+    list_display_links = ['title']
+    list_editable = ['leader']
+    ordering = ['title']
+    list_per_page = 10
+    list_filter = ['title', 'leader']
+    search_fields = ['title', 'leader__first_name', 'leader__last_name']
+    form = GroupFormAdmin
+
+    def view_on_site(self, obj):
+        return reverse('groups_edit', kwargs={'pk': obj.pk})
+    
 # Register your models here.
 
 admin.site.register(Student, StudentAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
