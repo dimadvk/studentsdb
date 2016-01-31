d7dc05e207ca8ffee6e7d34877ec0572d23ac61f
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-01-31 08:09:17 +0200

    change validation for leader field

--- students/views/groups.py
+++ students/views/groups.py
@@ -112,10 +112,11 @@ class GroupUpdateForm(GroupCreateForm):
         
     def clean_leader(self):
         """ Check if leader is in the same group """
-        queryset = Student.objects.filter(student_group=self.instance)
         new_leader = self.cleaned_data['leader']
-        if new_leader and new_leader not in queryset:
-            raise ValidationError(u"Студент не входить до даної групи!")
+        queryset = Student.objects.filter(pk=new_leader.pk, student_group=self.instance.pk)
+        if len(queryset) == 0:
+            raise ValidationError(u"Студент не входить до даної групи!",
+                code='invalid')
         return new_leader
  
 
