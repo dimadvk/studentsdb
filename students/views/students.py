6b74b6963e65f17866aeb8b117cf9db72a4f98c1
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-02-04 21:21:13 +0200

    p451

--- students/views/students.py
+++ students/views/students.py
@@ -13,6 +13,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 
 from ..models.student import Student
 from ..models.group import Group
+from ..util import paginate, get_current_group
 
 from students_add import StudentAddForm
 
@@ -90,14 +91,19 @@ class StudentList(ListView):
 
 
 def students_list(request):
-    students = Student.objects.all()
+    # check if need to show only one group of students
+    current_group = get_current_group(request)
+    if current_group:
+        students = Student.objects.filter(student_group=current_group)
+    else:
+        students = Student.objects.all()
 
     # try to order students_list
     order_by = request.GET.get('order_by', '')
     if order_by in ('last_name', 'first_name', 'ticket'):
         students = students.order_by(order_by)
-    if request.GET.get('reverse', '') == '1':
-        students = students.reverse()
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
     else:
        order_by = 'last_name'
        students = students.order_by(order_by)
@@ -118,34 +124,31 @@ def students_list(request):
 
     # remake pagination without 'paginator'
     # count of pages:
-    num_rows_per_page = 4
-    if len(students):
-        num_pages = len(students) // num_rows_per_page
-    else:
-        num_pages = 1
-    # if len(students) % num_rows_per_page > 0
-    if len(students) % num_rows_per_page:
-        num_pages += 1
-    page = request.GET.get('page')
-    # ceck if page is integer
-    try:
-        page = int(page)
-    except:
-        page = 1
-    # if page is out of range return last page
-    if page > num_pages:
-        page = num_pages
-    elif page < 1:
-        page = 1
-    students = students[ (page-1)*num_rows_per_page : page*num_rows_per_page ]
-    page_list = [p+1 for p in range(num_pages)]
-    return render(request, 'students/students_list.html',
-            {'students':students,
-            'page':page,
-            'num_pages':num_pages,
-            'num_rows_per_page':num_rows_per_page,
-            'page_list':page_list}
-            )
+    #num_rows_per_page = 4
+    #if len(students):
+    #    num_pages = len(students) // num_rows_per_page
+    #else:
+    #    num_pages = 1
+    ## if len(students) % num_rows_per_page > 0
+    #if len(students) % num_rows_per_page:
+    #    num_pages += 1
+    #page = request.GET.get('page')
+    ## ceck if page is integer
+    #try:
+    #    page = int(page)
+    #except:
+    #    page = 1
+    ## if page is out of range return last page
+    #if page > num_pages:
+    #    page = num_pages
+    #elif page < 1:
+    #    page = 1
+    #students = students[ (page-1)*num_rows_per_page : page*num_rows_per_page ]
+    #page_list = [p+1 for p in range(num_pages)]
+
+    context = paginate(students, 3, request, {}, var_name='students')
+
+    return render(request, 'students/students_list.html', context)
 
 def students_ajax_next_page(request):
     text = '<h2>Text from view students_ajax_next_page, received via ajax</h2>'
