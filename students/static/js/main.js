f9c983d922208127463dda983654937e74c9c308
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-02-05 15:02:45 +0200

    p473 - add form to modal window

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -59,11 +59,37 @@ function initDateFields() {
 
 function initEditStudentPage() {
     $('a.student-edit-form-link').click(function(event){
-        var modal = $('#myModal');
-        modal.modal('show');
+        var link = ${this);
+        $.ajax({
+            'url': link.attr('href'),
+            'dataType': 'html',
+            'type': 'get',
+            'success': function(data, status, xhr){
+                // check if we got successfull response from the server
+                if (status != 'success') {
+                    alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+                    return false;
+                }
+
+                // update modal window with arrived content from the server
+                var modal = $('#myModal');
+                html = $(data), form = html.find('#content-column form');
+                modal.find('.modal-title').html(html.find('#content-column h2').text());
+                modal.find('.modal-body').html(form);
+
+                // setup and show modal window finally
+                modal.modal('show');
+            },
+            'error': function(){
+                alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+                return false;
+            }
+        });
+
         return false;
     });
 }
+
 
 
 $(document).ready(function(){
