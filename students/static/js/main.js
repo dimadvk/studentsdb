49273d93df0e9cdb549ad555222dc6690d537b7c
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-02-05 13:20:17 +0200

    add js initial modal window

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -57,10 +57,19 @@ function initDateFields() {
     });
 }
 
+function initEditStudentPage() {
+    $('a.student-edit-form-link').click(function(event){
+        var modal = $('#myModal');
+        modal.modal('show');
+        return false;
+    });
+}
+
 
 $(document).ready(function(){
     initJournal();
     initGroupSelector();
     initDateFields();
+    initEditStudentPage();
 }
 );
