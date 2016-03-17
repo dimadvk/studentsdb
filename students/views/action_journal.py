bffa95fc5245ae29c0e9593a62fc69f58d2f64a6
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-03-17 13:50:20 +0200

    just del comment

--- students/views/action_journal.py
+++ students/views/action_journal.py
@@ -12,7 +12,6 @@ class ActionListView(ListView):
     context_object_name = 'actions'
 
     def get_context_data(self, **kwargs):
-        # get context dara from TemplateView class
         context = super(ActionListView, self).get_context_data(**kwargs)
         context = paginate(self.queryset, 10, self.request, context, var_name='actions')
         return context
