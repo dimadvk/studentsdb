098a4587910dde9e63264544668c236731e8bff7
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-03-23 17:02:36 +0200

    change solution for setting language

--- students/views/set_language.py
+++ students/views/set_language.py
@@ -1,18 +1,24 @@
 from django.http import HttpResponseRedirect
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 
 def set_language(request):
-    # get path refferer
-    return_path = request.GET.get('return-path', '/')
-    response = HttpResponseRedirect(return_path)
-    # get lang_code of selected language
-    selected_language = request.GET.get('lang', 'en')
-    # set cookie
-    max_age = 10*365*24*60*60 # ten years
-    response.set_cookie(
-        settings.LANGUAGE_COOKIE_NAME,
-        selected_language,
-        max_age=max_age)
+    if request.method == "POST":
+        # get path refferer
+        return_path = request.POST.get('return-path', reverse('home'))
+        print '-----------------', return_path
+        response = HttpResponseRedirect(return_path)
+        # get lang_code of selected language
+        selected_language = request.POST.get('lang', settings.LANGUAGE_CODE)
+        print '===========', selected_language
+        # set cookie
+        max_age = 10*365*24*60*60 # ten years
+        response.set_cookie(
+            settings.LANGUAGE_COOKIE_NAME,
+            selected_language,
+            max_age=max_age)
+    else:
+        response = HttpResponseRedirect(reverse('home'))
     return response
 
