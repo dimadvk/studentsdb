3845ed032428d6757708bf8fb0c430b923b03e28
Dimitriy Kutnyakov <ren-kpi@i.ua>
2016-04-14 16:08:41 +0300

    make command localize_static - in process...

--- students/management/commands/localize_static.py
+++ students/management/commands/localize_static.py
@@ -16,19 +16,16 @@ replace external links to local""")
 
     APP_NAME = 'students'
     TEMPLATE_NAME = 'base.html'
-    PATH_DIR_STATIC_CSS = os.path.join(settings.BASE_DIR, APP_NAME, 'static_test', 'css')
-    PATH_DIR_STATIC_JS = os.path.join(settings.BASE_DIR, APP_NAME, 'static_test', 'js')
+    PATH_DIR_STATIC_CSS = os.path.join(settings.BASE_DIR, APP_NAME, 'static', 'css')
+    PATH_DIR_STATIC_JS = os.path.join(settings.BASE_DIR, APP_NAME, 'static', 'js')
     PATH_TEMPLATE = os.path.join(settings.BASE_DIR, APP_NAME, 'templates', APP_NAME, TEMPLATE_NAME)
 
     def handle(self, *args, **options):
-        pass
 
-        # read template to string, make it unicode
-        template_str = open(self.PATH_TEMPLATE).read()
-        template_str = template_str.decode('utf-8')
-
-        # make the soup
-        soup = BeautifulSoup(template_str, 'html.parser')
+        # read template to string, make it unicode, and make the soup
+        html_template = open(self.PATH_TEMPLATE).read()
+        html_template = html_template.decode('utf-8')
+        soup = BeautifulSoup(html_template, 'html.parser')
 
         # find all tags: <link rel='stylesheet'>, <script>
         # get all links
@@ -42,27 +39,29 @@ replace external links to local""")
 
         # download all the files to <app_name>/static
         # show progress of downloading
+        print "Start downloading files:"
         for url in urls_static_css:
             result = self._download_static_file(url, self.PATH_DIR_STATIC_CSS)
-            print _(u"Download file: %(filename)s; status: %(status)s" % result)
+            print _(u"Get file: %(filename)s; status: %(status)s" % result)
         for url in urls_static_js:
             result = self._download_static_file(url, self.PATH_DIR_STATIC_JS)
-            print _(u"Download file: %(filename)s; status: %(status)s" % result)
+            print _(u"Get file: %(filename)s; status: %(status)s" % result)
 
         # make middleware for replacement:
         # replace all urls in soup - external to local
 
     def _download_static_file(self, url, dir_path):
         filename = url.split('/')[-1]
-        local_file = dir_path + filename
-        response = requests.get(url, verify=False, stream=True)
-        if response.ok:
+        local_file = os.path.join(dir_path, filename)
+        requests.packages.urllib3.disable_warnings() # God blessed me
+        try:
+            response = requests.get(url, verify=False, stream=True, timeout=10)
             with open(local_file, 'wb') as f:
                 for chunk in response.iter_content(chunk_size=1024):
                     if chunk:
                         f.write(chunk)
                 f.close()
             download_status = 'OK'
-        else:
-            download_status = 'BAD'
+        except:
+            download_status = 'ERROR'
         return {'filename': filename, 'status': download_status}
